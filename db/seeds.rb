d3c900f51a12c6d4303d644438341627d7502f93
pgolez <paul.golez@codingavenue.com>
2020-05-04 17:46:23 +0800

    Initial seeds for courses and course_classess

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+courses = Course.create [
+  {code: 'IT-110', descriptive_title: 'IT Fundamentals', units: 3},
+  {code: 'IT-111', descriptive_title: 'Program Logic Design', units: 4}
+]
+
+course_classes = CourseClass.create [
+  {code: '69923', max_enrollment: 25, course_id: courses[0].id},
+  {code: '68078', max_enrollment: 25, course_id: courses[0].id},
+]

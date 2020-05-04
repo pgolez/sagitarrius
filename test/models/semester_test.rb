027e371ad277d097b1acbc95abbd41d283ea25f8
pgolez <paul.golez@codingavenue.com>
2020-05-04 20:50:04 +0800

    Add test for semester model

--- test/models/semester_test.rb
+++ test/models/semester_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SemesterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the name method" do
+    assert_equal 'First Semester S.Y. 2020-2021', semesters(:first_semester_2020_2021).name
+  end
 end

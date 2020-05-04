c7bedca7410dd7be4357edbbdf8e941b2a48eac0
pgolez <paul.golez@codingavenue.com>
2020-05-04 20:49:42 +0800

    Create unit test for rooms

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the name method" do
+    assert_equal 'Forensics Laboratory', rooms(:forensics_lab).name
+  end
 end

bf293b875d1dce189627d0d9e5605d74d4c632e0
pgolez <paul.golez@codingavenue.com>
2020-05-05 10:27:23 +0800

    Implement endpoint for saving course class

--- app/controllers/course_classes_controller.rb
+++ app/controllers/course_classes_controller.rb
@@ -1,4 +1,5 @@
 class CourseClassesController < ApplicationController
+
 	def index
 		@course_classes = CourseClass.all
 	end
@@ -6,4 +7,18 @@ class CourseClassesController < ApplicationController
 	def show
 		@course_class = CourseClass.find(params[:id])
 	end
+
+	def create
+		@course_class = CourseClass.new course_class_params
+		@course_class.DRAFT!
+		@course_class.save
+	end
+
+	private
+
+	def course_class_params
+		params.require(:course_class)
+		.permit(:code, :max_enrollment, :course_id, :room_id, :semester_id)
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,9 @@ semester = Semester.create name: 'First Semester 2020-2021'
 
 course_classes = CourseClass.create [
   {code: '69923', max_enrollment: 25, course_id: courses[0].id,
-  room_id: rooms[0].id, semester_id: semester.id, status: :open},
+  room_id: rooms[0].id, semester_id: semester.id, status: :OPEN},
   {code: '68078', max_enrollment: 25, course_id: courses[0].id,
-  room_id: rooms[1].id, semester_id: semester.id, status: :open},
+  room_id: rooms[1].id, semester_id: semester.id, status: :OPEN},
 ]
 
 pp course_classes[0].status

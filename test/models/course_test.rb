0f94120f28876299bd8906dc2f2146a5f180fc3d
pgolez <paul.golez@codingavenue.com>
2020-05-05 09:02:49 +0800

    Add unit test to Course model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the code method" do
+    assert_equal 'IT-110', courses(:it_110).code
+  end
+
+  test "the descriptive_title method" do
+    assert_equal 'IT Fundamentals', courses(:it_110).descriptive_title
+  end
+
+  test "the units method" do
+    assert_equal 3, courses(:it_110).units
+  end
 end

6b31cc44f9d02ade49a024cc3d563ea546563841
pgolez <paul.golez@codingavenue.com>
2020-05-05 18:38:05 +0800

    Add functional tests on  GET methods of CourseClassesController

--- test/controllers/course_classes_controller_test.rb
+++ test/controllers/course_classes_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class CourseClassesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should list all classes" do
+    get '/course-classes', headers: {'accept': 'application/json'}
+
+    assert_response :success
+    assert_instance_of(Array, @response.parsed_body)
+    assert_equal(2, @response.parsed_body.size)
+    assert_course_class_list_json_structure(@response.parsed_body)
+  end
+
+  test "should show specific class" do
+    id = course_classes(:course_632258).id
+    get "/course-classes/#{id}", headers: {'accept': 'application/json'}, params: {id: 1}
+
+    assert_response :success
+    assert_course_class_json_structure(@response.parsed_body)
+  end
+
+  private
+
+  def assert_course_class_list_json_structure (course_classes)
+    course_classes.each do |course_class|
+      assert_course_class_json_structure(course_class)
+    end
+  end
+
+  def assert_course_class_json_structure (course_class)
+    ['id', 'code', 'course', 'room', 'semester',
+    'max_enrollment', 'status'].each do |key|
+      assert_key_exists(course_class, key)
+    end
+  end
+
+  def assert_key_exists(course_class, key)
+    assert course_class.has_key?(key), "Missing #{key} in course class JSON response"
+  end
+
 end

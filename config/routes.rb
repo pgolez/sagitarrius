65c36f14978d94201713841304b4d0c224e03ead
pgolez <paul.golez@codingavenue.com>
2020-05-08 11:21:40 +0800

    Create endpoint to list all rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :course_classes, only: [:index, :show, :create, :update], path: 'course-classes'
+  resources :rooms, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
